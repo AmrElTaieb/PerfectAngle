074f06949d47867db339b53971d76878a827bd52
Tarek <midot222@gmail.com>
2019-05-29 13:08:53 +0200

    Added detecting larget countour

--- app/src/main/java/iti/edge/MainActivity.java
+++ app/src/main/java/iti/edge/MainActivity.java
@@ -12,11 +12,22 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
     Mat mRgba;
     Mat mRgbaF;
     Mat mRgbaT;
+
     protected CameraBridgeViewBase mCamera;
 
     //Create load callback
@@ -41,7 +52,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     public void onResume()
     {
         super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
     }
 
     @Override
@@ -71,39 +82,46 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        double maxArea=0;
+        Point first = new Point(0,0) ,second = new Point(0,0) ;
 
-//        Mat edges = new Mat();
-//        List<MatOfPoint> contours = new ArrayList<>();
-//        mRgba = inputFrame.rgba();
-//        Imgproc.cvtColor(mRgba,  mRgba, Imgproc.COLOR_BGR2GRAY);
-//        Imgproc.GaussianBlur(mRgba, mRgba, new Size(5, 5), 0);
-//          Imgproc.Canny(mRgba, mRgba, 10, 30);
-//        Imgproc.findContours(mRgba,contours,edges,Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_SIMPLE);
-//
-//
-//        Iterator<MatOfPoint> iterator = contours.iterator();
-//        while (iterator.hasNext()){
-//            MatOfPoint contour = iterator.next();
-//            Rect rect ;
-//            rect = Imgproc.boundingRect(contour);
-//            //  Imgproc.rectangle(mRgba,new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(255.0,255.0,255.0));
-//
+        Mat edges = new Mat();
+        List<MatOfPoint> contours = new ArrayList<>();
+        mRgba = inputFrame.rgba();
+        Imgproc.cvtColor(mRgba,  mRgba, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.GaussianBlur(mRgba, mRgba, new Size(5, 5), 0);
+        Imgproc.Canny(mRgba, mRgba, 1, 20);
+        Imgproc.findContours(mRgba,contours,edges,Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+        Iterator<MatOfPoint> iterator = contours.iterator();
+        Rect rect ;
+        while (iterator.hasNext()){
+            MatOfPoint contour = iterator.next();
+            rect = Imgproc.boundingRect(contour);
+             if(rect.area() > maxArea)
+             {
+                 maxArea = rect.area() ;
+                 first  = new Point(rect.x,rect.y);
+                 second = new Point(rect.x+rect.width,rect.y+rect.height);
+
+                 Log.i("Condition","Entered If Condition");
+             }
+
+             Log.i("iteration","Loop has passed");
 //            Imgproc.circle(mRgba,new Point(rect.x,rect.y),5,new Scalar(200,0,0),2);
 //            Imgproc.circle(mRgba,new Point(rect.x+rect.width,rect.y),5,new Scalar(200,0,0),2);
 //            Imgproc.circle(mRgba,new Point(rect.x,rect.y+rect.height),5,new Scalar(200,0,0),2);
 //            Imgproc.circle(mRgba,new Point(rect.x+rect.width,rect.y+rect.height),5,new Scalar(200,0,0),2);
-//
-//
-//        }
-//
-//
-//
-//
-//
-//
-//        mRgba.convertTo(mRgbaF,CvType.CV_8U);
+        }
+        Log.i("finish","Loop has finished");
+        Imgproc.rectangle(mRgba,first, second,new Scalar(255.0,255.0,255.0));
+
+
+
+       mRgba.convertTo(mRgbaF,CvType.CV_8U);
 //        //Return result
-        return inputFrame.rgba();
+        return mRgbaF;
 
     }
 
