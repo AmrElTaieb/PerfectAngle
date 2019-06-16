4092594e246b474b0f9b34297f0cdcb182b9f796
DoaaTantawy <doaaahmed_55@yahoo.com>
2019-06-16 23:33:11 +0200

    changing constraint layout into Linear layout in login screen

--- OpenCV3.1/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ OpenCV3.1/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\ITI\\PerfectAngle\\OpenCV3.1\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\doaaa\\PerfectAngle\\OpenCV3.1\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
